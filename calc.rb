e45193f52f532b6821534d7a4501545fc7487753
KatzStr <madderladder@gmail.com>
2017-09-18 13:47:18 +0900

    first commit

---
+++ calc.rb
@@ -0,0 +1,19 @@
+
+
+def calc(x, y, operator)
+  if operator == "+"
+    puts x + y
+  elsif operator == "-"
+    puts x - y
+  elsif operator == "*"
+    puts x * y
+  elsif operator == "/"
+    if y == 0
+      puts "ゼロ除算エラーです"
+    else
+      puts x / y
+    end
+  else
+    puts "不正な演算子です"
+  end
+end
